d237ddc8ab08bb2c6a10329bf322c32950d79c57
richard <richard.allexzandre.7@gmail.com>
2026-04-03 15:46:16 -0400

    exercicio 5

--- src/Carro.java
+++ src/Carro.java
@@ -1,17 +1,20 @@
 public class Carro {
-    public String marca;
-    public String modelo;
-    public int ano;
+    private String marca;
+    private String modelo;
+    private int ano;
 
-    public String getMarca() {
-        return marca;
+    public Carro(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        setAno(ano);
     }
-    public String getModelo() {
-        return modelo;
-    }
-    public int getAno() {
-        return ano;
-    }
+    public String getMarca()  { return marca; }
+    public String getModelo() { return modelo; }
+    public int getAno()       { return ano; }
+
+    public void setMarca(String marca)   { this.marca = marca; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
+
     public void setAno(int ano) {
         if (ano < 1886 || ano > 2026) {
             System.out.println("Erro: ano inválido! O ano deve ser entre 1886 e 2026.");
@@ -19,11 +22,13 @@ public class Carro {
             this.ano = ano;
         }
     }
+
     public void exibir() {
         System.out.println("Marca: " + getMarca());
         System.out.println("Modelo: " + getModelo());
         System.out.println("Ano: " + getAno());
     }
+
     public int idadeDoCarro() {
         return 2026 - getAno();
     }
--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,9 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        Carro carro1 = new Carro();
-        carro1.marca = "Toyota";
-        carro1.modelo = "Corolla";
-        carro1.ano = 2022;
-
-        Carro carro2 = new Carro();
-        carro2.marca = "Honda";
-        carro2.modelo = "Civic";
-        carro2.ano = 2020;
-
-        Carro carro3 = new Carro();
-        carro3.marca = "Ford";
-        carro3.modelo = "Ka";
-        carro3.ano = 2017;
-
-        System.out.println("=== Teste de validação ===");
-        carro1.setAno(1500);
-        carro1.setAno(3000);
-        System.out.println();
+        Carro carro1 = new Carro("Toyota", "Corolla", 2020);
+        Carro carro2 = new Carro("Honda", "Civic", 2022);
+        Carro carro3 = new Carro("Ford", "Ka", 2018);
 
         ArrayList<Carro> carros = new ArrayList<>();
         carros.add(carro1);
