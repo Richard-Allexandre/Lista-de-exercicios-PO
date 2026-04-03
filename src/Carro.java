c2bee660bbcfcaa1180fcbde091dc35b7daa3441
richard <richard.allexzandre.7@gmail.com>
2026-04-03 14:37:20 -0400

    exercicio 1

--- src/Carro.java
+++ src/Carro.java
@@ -3,16 +3,5 @@ public class Carro {
     public String modelo;
     public int ano;
 
-    // metodo 1 - void (só executa, não devolve nada)
-    void  exibir() {
-        System.out.println("Marca: " + marca);
-        System.out.println("Modelo: " + modelo);
-        System.out.println("Ano: " + ano);
-    }
-
-    // metodo 2 - com retorno int (devolve um numero)
-    int idadeDoCarro() {
-        return 2026 - ano;
-    }
 }
 
---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Carro carro1 = new Carro();
+        carro1.marca = "Toyota";
+        carro1.modelo = "Corolla";
+        carro1.ano = 2022;
+
+        Carro carro2 = new Carro();
+        carro2.marca = "Honda";
+        carro2.modelo = "Civic";
+        carro2.ano = 2020;
+
+        System.out.println("=== Carro 1 ===");
+        System.out.println("Marca: " + carro1.marca);
+        System.out.println("Modelo: " + carro1.modelo);
+        System.out.println("Ano: " + carro1.ano);
+
+
+        System.out.println();
+
+
+        System.out.println("=== Carro 2 ===");
+        System.out.println("Marca: " + carro2.marca);
+        System.out.println("Modelo: " + carro2.modelo);
+        System.out.println("Ano: " + carro2.ano);
+
+    }
+
+}
